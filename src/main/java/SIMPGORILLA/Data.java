3e8a3650651a90e2c77281da8cbc5d04ecffd6b8
BuildTools <unconfigured@null.spigotmc.org>
2022-01-03 14:31:09 +0100

    Data og Player

--- src/main/java/SIMPGORILLA/Data.java
+++ src/main/java/SIMPGORILLA/Data.java
@@ -9,52 +9,37 @@ public class Data {
 
     private static Player player1 = new Player(0, 0, true);
     private static Player player2 = new Player(CANVAS_X - 1, 0, false);
+    private static Player proj = new Player(0,0,false);
 
     public static void main(String[] args) {
-
-
-
-
-
         simulateProjectile(player1, player2,45, 25);
-
     }
 
     public static void simulateProjectile(Player playerThatShoots, Player playThatIsHit, int a, double velocity) {
         double angle = Math.toRadians(a);
-        double y = playerThatShoots.getY();
-        double x = playerThatShoots.getX();
+        proj.setY(playerThatShoots.getY());
+        proj.setX(playerThatShoots.getX());
 
-        while (pointIsInside(x,y)) {
-            y = g / (2 * Math.pow(velocity, 2) * Math.pow(Math.cos(angle), 2)) * Math.pow(x, 2) + Math.tan(angle) * x;
-            System.out.println("x:"+x+" y:"+y);
+        while (projIsInside(proj.getX(),proj.getY())) {
+            proj.setY(g / (2 * Math.pow(velocity, 2) * Math.pow(Math.cos(angle), 2)) * Math.pow(proj.getX(), 2) + Math.tan(angle) * proj.getX());
+            System.out.println(proj.toString());
 
-            if (playerIsHit(playThatIsHit, x, y)){
+            if (playerIsHit(playThatIsHit)){
                 System.out.println(" player is hit!");
                 break;
             }
-            x += 0.1;
+            proj.addToX(0.1);
         }
-
     }
 
-    public static boolean pointIsInside(double x, double y) {
+    public static boolean projIsInside(double x, double y) {
         return (x < CANVAS_X && x >= 0 && y >= 0);
     }
 
-    public static boolean playerIsHit(Player player, double x, double y){
-        double len = player2.distanceToProjectile(x,y);
+    public static boolean playerIsHit(Player player){
+        double len = player.distanceToProjectile(proj.getX(), proj.getY());
         System.out.print(" len:" + len);
-
-        if(len < 50){
-            return true;
-        }
-        return false;
-
-
-
-
-
+        return len < 50;
     }
 
 
