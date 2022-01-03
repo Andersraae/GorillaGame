af8a8a86ca5aa8c23db9a6da9ae1bbb16e4c3cf0
BuildTools <unconfigured@null.spigotmc.org>
2022-01-03 14:47:59 +0100

    Data og Player

--- src/main/java/SIMPGORILLA/Data.java
+++ src/main/java/SIMPGORILLA/Data.java
@@ -6,13 +6,15 @@ public class Data {
     private static final int CANVAS_X = 100;
     private static final int CANVAS_Y = 50;
     private static final double g = -9.81;
+    private static final int VELOCITY = 25;
+    private static final int ANGLE_IN_DEGREES = 45;
 
     private static Player player1 = new Player(0, 0, true);
     private static Player player2 = new Player(CANVAS_X - 1, 0, false);
     private static Player proj = new Player(0,0,false);
 
     public static void main(String[] args) {
-        simulateProjectile(player1, player2,45, 25);
+        simulateProjectile(player1, player2,ANGLE_IN_DEGREES, VELOCITY);
     }
 
     public static void simulateProjectile(Player playerThatShoots, Player playThatIsHit, int a, double velocity) {
@@ -30,6 +32,11 @@ public class Data {
             }
             proj.addToX(0.1);
         }
+        System.out.println("time: " + returnTime() + " s");
+    }
+
+    public static double returnTime(){
+        return (2*VELOCITY*Math.sin(Math.toRadians(ANGLE_IN_DEGREES)))/-g;
     }
 
     public static boolean projIsInside(double x, double y) {
@@ -38,7 +45,7 @@ public class Data {
 
     public static boolean playerIsHit(Player player){
         double len = player.distanceToProjectile(proj.getX(), proj.getY());
-        System.out.print(" len:" + len);
+        System.out.print("len:" + len);
         return len < 50;
     }
 
