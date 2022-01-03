b057c94886570d02d0593dd54a36a6bae785311d
Andersraae <andersraae01@gmail.com>
2022-01-03 13:36:30 +0100

    Fix

--- src/main/java/SIMPGORILLA/GameApplication.java
+++ src/main/java/SIMPGORILLA/GameApplication.java
@@ -10,7 +10,9 @@ import java.io.IOException;
 public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        System.out.println("før load");
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("game-view.fxml"));
+        System.out.println("Efter load");
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -18,6 +20,7 @@ public class GameApplication extends Application {
     }
 
     public static void main(String[] args) {
+        System.out.println("øh");
         launch();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,8 @@
-module com.example.gorillagame {
+module SIMPGORILLA.gorillagame {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens example.gorillagame to javafx.fxml;
-    exports example.gorillagame;
-    exports SIMPGORILLA;
     opens SIMPGORILLA to javafx.fxml;
+    exports SIMPGORILLA;
 }
