bb591a4e02357a9ee252adc6ad0382ab4c0df674
Andersraae <andersraae01@gmail.com>
2022-01-04 09:09:24 +0100

    Noget med kast

--- src/main/java/SIMPGORILLA/GameController.java
+++ src/main/java/SIMPGORILLA/GameController.java
@@ -7,8 +7,11 @@ import javafx.scene.shape.Circle;
 
 public class GameController  {
     private static final int CANVAS_X = 100;
-    private static Player player1 = new Player(0, 0, true);
-    private static Player player2 = new Player(CANVAS_X - 1, 0, false);
+    private static Player player1 = new Player(0, 400, true);
+    private static Player player2 = new Player(CANVAS_X - 1, 400, false);
+    private static Player proj = new Player(0,0,false);
+    private static final double g = 9.81;
+    private static final int STEPS = 100;
 
     @FXML
     private Circle projectile;
@@ -17,24 +20,51 @@ public class GameController  {
     @FXML
     private TextField velocity;
 
+
     public void kast(){
-        projectile.setCenterX(100);
-
-        System.out.println(projectile.getCenterX());
-        AnimationTimer animation = new AnimationTimer() {
-            @Override
-            public void handle(long l) { //l er den nuværende tid
-
-            }
-        };
-
         if(angle.getText() != null && velocity.getText() != null){
             double numangle = Double.parseDouble(angle.getText());
             double numvelocity = Double.parseDouble(velocity.getText());
-            Data.simulateProjectileWithTime(player1, player2, numangle, numvelocity);
-
-            animation.start();
-
+            simulateProjectileWithTime(player1, player2, numangle, numvelocity);
         }
+    }
+
+    public void simulateProjectileWithTime(Player shootingPlayer, Player targetPlayer, double ANGLE_IN_DEGREES, double VELOCITY){
+        double angle = Math.toRadians(ANGLE_IN_DEGREES);
+        double xVelocity = VELOCITY * Math.cos(angle);
+        double yVelocity = VELOCITY * Math.sin(angle);
+        double totalTime = - 2.0 * yVelocity / -g;
+        double timeIncrement = totalTime / STEPS;
+        double xIncrement = xVelocity * timeIncrement;
+
+        double x = shootingPlayer.getX();
+        double y = shootingPlayer.getY();
+        double t = 0.0;
+        System.out.println("step\tx \t y \t time \t length");
+        System.out.println("0\t0.0\t\t0.0\t\t0.0");
+
+        for (int i = 1; i <= STEPS; i++) {
+            t += timeIncrement;
+            x += xIncrement;
+            y = yVelocity * t + 0.5 * -g * t * t;
+            proj.setX(x);
+            proj.setY(y);
+            projectile.setCenterX(x);
+            projectile.setCenterY(x);
+
+            double l = player2.distanceToProjectile(proj.getX(), proj.getY());
+
+            System.out.println(i + "\t" + round(x) + "\t" + round(y) + "\t" + round(t) + "\t" + round(l));
+            }
+        }
+
+    public static String round(double a){
+        return String.format("%.2f",a);
+    }
+
+    public static boolean playerIsHit(Player player){
+        double len = player.distanceToProjectile(proj.getX(), proj.getY());
+        //System.out.print("len:" + round(len) + " ");
+        return len < 50;
     }
 }
